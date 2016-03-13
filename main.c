0171a867c764b5459eee18eb2d10153f8c838d10
Nicolas LE BRETON <nle-bret@e1r6p21.42.fr>
2016-03-13 07:40:42 +0100

    LOOOOOOL

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
 #include "libft.h"
 #include <stdio.h>
 
+typedef struct 	s_pos
+{
+	int			a;
+	int			b;
+	char		*s;
+}				t_pos;
+
 typedef struct  s_data
 {
 	char		*string;
 	int			x;
 	int			z;
+	t_pos		p;
 }				t_data;
 
-t_data	*ft_create_data(char *str, int x, int z)
+t_data	ft_create_data(char *str, int x, int z)
 {
-	t_data	*d;
+	t_data	d;
 
-	if ((d = (t_data*)ft_memalloc(sizeof(t_data))) == NULL)
-		return (NULL);
-	d->string = ft_strdup(str);
-	d->x = x;
-	d->z = z;
+	d.string = ft_strdup(str);
+	d.x = x;
+	d.z = z;
+	d.p.a = 10;
+	d.p.b = z;
+	d.p.s = d.string;
 	return (d);
 }
 
@@ -30,30 +39,34 @@ void	ft_print_data(void *ptr)
 	ft_putnbr(d->x);
 	ft_putchar(' ');
 	ft_putnbr(d->z);
+	ft_putchar(' ');
+	ft_putnbr(d->p.a);
+	ft_putchar(' ');
+	ft_putnbr(d->p.b);
+	ft_putchar(' ');
+	ft_putstr(d->string);
 }
 
 int		main(int ac, char **av)
 {
 	int		i;
 	t_list	*lst;
-	t_data	*tmp;
+	t_data	tmp;
 
 	lst = NULL;
 	i = 1;
-	tmp = NULL;
 	if (ac > 1)
 	{
 		while (av[i])
 		{
 			tmp = ft_create_data(av[i], 10, i);
-			ft_lstpush_front(&lst, tmp, sizeof(t_data));
-			ft_memdel((void*)&tmp);
+			ft_lstpush_front(&lst, &tmp, sizeof(t_data));
 			i++;
 		}
 		tmp = ft_create_data("aasd;lasdajd", 1, 223432423);
-		ft_lstadd(&lst, ft_lstnew(tmp, sizeof(t_data)));
-		ft_memdel((void*)&tmp);
+		ft_lstadd(&lst, ft_lstnew(&tmp, sizeof(t_data)));
 		ft_lstprint(lst, ft_print_data);
+
 	}
 	//sleep(2);
 	return (0);
