d6ad5475be84590d6834a47d836e11b1c01366bb
Nicolas LE BRETON <nle-bret@e1r4p6.42.fr>
2016-03-28 01:23:48 +0200

    if simple lst

--- srcs/list/ft_lstprint.c
+++ srcs/list/ft_lstprint.c
@@ -17,7 +17,10 @@ void	ft_lstprint(t_list *lst, void (*print)(void *))
 	while (lst)
 	{
 		ft_putstr("\033[033mcontent : \033[0m[");
-		(*print)(lst->content);
+		if (print)
+			(*print)(lst->content);
+		else
+			ft_putstr(lst->content);
 		ft_putstr("]\t\033[035msize : \033[0m[");
 		ft_putnbr(lst->content_size);
 		ft_putstr("]\n");
