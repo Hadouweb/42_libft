1dff486af8c4b5b817b994204abe06d690c2a574
Hadouweb <kengo14@hotmail.fr>
2016-10-24 17:19:09 +0200

    Update libft

--- srcs/str/ft_stradd_char.c
+++ srcs/str/ft_stradd_char.c
@@ -20,7 +20,6 @@ char	*ft_stradd_char(char **s, char c)
 	len = ft_strlen(*s);
 	str = (char *)malloc(len + 1 * sizeof(char) + 1);
 	ft_strcpy(str, *s);
-	ft_strdel(s);
 	str[len++] = c;
 	str[len] = '\0';
 	return (str);
