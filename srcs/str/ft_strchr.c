4fea4c192dba6c26d09c3a72f1c3c275ddb077b4
Hadouweb <kengo14@hotmail.fr>
2016-11-08 23:26:12 +0100

    Norm

--- srcs/str/ft_strchr.c
+++ srcs/str/ft_strchr.c
@@ -19,12 +19,12 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	if (s)
 	{
-		if ((char) c == '\0')
-			return ((char *) (s + ft_strlen(s)));
+		if ((char)c == '\0')
+			return ((char *)(s + ft_strlen(s)));
 		while (s[i])
 		{
-			if (s[i] == (char) c)
-				return ((char *) (s + i));
+			if (s[i] == (char)c)
+				return ((char *)(s + i));
 			i++;
 		}
 	}
