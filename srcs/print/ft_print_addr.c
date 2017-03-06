2038014838dad43f0c1e8de1ad36020efc0e6a2a
Hadouweb <kengo14@hotmail.fr>
2017-03-06 03:41:00 +0100

    Update

--- srcs/print/ft_print_addr.c
+++ srcs/print/ft_print_addr.c
@@ -16,19 +16,9 @@ void	ft_print_addr(void *ptr)
 {
 	char			*str;
 	unsigned long	n;
-	char			output[13];
-	int				i;
 
 	n = (unsigned long)ptr;
 	str = ft_ulltoa_base(n, 16);
-	output[0] = '0';
-	output[1] = 'x';
-	i = 2;
-	while (i < 12)
-	{
-		output[i] = str[i - 2];
-		i++;
-	}
-	output[i] = '\0';
-	write(1, output, 12);
+	write(1, "0X", 2);
+	write(1, str, ft_strlen(str));
 }
