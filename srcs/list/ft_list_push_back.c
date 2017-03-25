b36d6e49ae46763573d375456cf53c32b834bd1a
Nicolas LE BRETON <nle-bret@e1r5p19.42.fr>
2017-03-25 02:10:14 +0100

    Correction push front

--- srcs/list/ft_list_push_back.c
+++ srcs/list/ft_list_push_back.c
@@ -6,7 +6,7 @@
 /*   By: nle-bret <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 05:00:08 by nle-bret          #+#    #+#             */
-/*   Updated: 2016/12/13 05:00:14 by nle-bret         ###   ########.fr       */
+/*   Updated: 2017/03/25 02:10:02 by nle-bret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_list_push_back(t_list *list, t_link *link)
 {
-	if (link == NULL)
+	if (link == NULL || list == NULL)
 		return ;
 	if (list->head == NULL)
 	{
--- srcs/list/ft_list_push_front.c
+++ srcs/list/ft_list_push_front.c
@@ -6,7 +6,7 @@
 /*   By: nle-bret <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 05:00:24 by nle-bret          #+#    #+#             */
-/*   Updated: 2016/12/13 05:00:25 by nle-bret         ###   ########.fr       */
+/*   Updated: 2017/03/25 02:10:01 by nle-bret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_list_push_front(t_list *list, t_link *link)
 		list->size++;
 		return ;
 	}
+	list->head->prev = link;
 	link->next = list->head;
 	list->head = link;
 	list->size++;
